79fe4a18eb61b6864da24e3795cdb22420b9fae7
Nicole <nicole@atv14.com>
2024-05-01 21:46:13 -0300

    Inclusao do Controller em Produto e Pedido

--- src/main/java/br/com/descomplica/projeto/Controller/PedidoController.java
+++ src/main/java/br/com/descomplica/projeto/Controller/PedidoController.java
@@ -1,5 +1,48 @@
 package br.com.descomplica.projeto.Controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import br.com.descomplica.projeto.Entidade.Pedido;
+import br.com.descomplica.projeto.Servico.PedidoService;
+
+@RestController
+@RequestMapping("/pedido")
 public class PedidoController {
-    
+
+    @Autowired
+    PedidoService pedidoService;
+
+    // Endpoint para obter todos os pedidos
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Pedido> getAllPedidos() {
+        return pedidoService.getAll();
+    }
+
+    // Endpoint para obter um pedido por ID
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Pedido getPedidoById(@RequestParam Integer id) {
+        return pedidoService.getById(id);
+    }
+
+    // Endpoint para salvar um pedido
+    @RequestMapping(method = RequestMethod.POST)
+    public Pedido savePedido(@RequestBody Pedido pedido) {
+        return pedidoService.savePedido(pedido);
+    }
+
+    // Endpoint para atualizar um pedido
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public Pedido updatePedido(@RequestParam Integer id, @RequestBody Pedido pedido) {
+        return pedidoService.updatePedido(id, pedido);
+    }
+
+    // Endpoint para excluir um pedido
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public boolean deletePedido(@RequestParam Integer id) {
+        return pedidoService.deletePedido(id);
+    }
 }
--- src/main/java/br/com/descomplica/projeto/Controller/ProdutoController.java
+++ src/main/java/br/com/descomplica/projeto/Controller/ProdutoController.java
@@ -1,5 +1,48 @@
 package br.com.descomplica.projeto.Controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import br.com.descomplica.projeto.Entidade.Produto;
+import br.com.descomplica.projeto.Servico.ProdutoService;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
-    
+
+    @Autowired
+    ProdutoService produtoService;
+
+    // Endpoint para obter todos os produtos
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Produto> getAllProdutos() {
+        return produtoService.getAll();
+    }
+
+    // Endpoint para obter um produto por ID
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Produto getProdutoById(@RequestParam Integer id) {
+        return produtoService.getById(id);
+    }
+
+    // Endpoint para salvar um produto
+    @RequestMapping(method = RequestMethod.POST)
+    public Produto saveProduto(@RequestBody Produto produto) {
+        return produtoService.saveProduto(produto);
+    }
+
+    // Endpoint para atualizar um produto
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public Produto updateProduto(@RequestParam Integer id, @RequestBody Produto produto) {
+        return produtoService.updateProduto(id, produto);
+    }
+
+    // Endpoint para excluir um produto
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public boolean deleteProduto(@RequestParam Integer id) {
+        return produtoService.deleteProduto(id);
+    }
 }
